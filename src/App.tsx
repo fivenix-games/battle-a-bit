9837d4c6f69eb57ed281e36ea35056cbd63d7d87
Fivenix Games <fivenix.games@gmail.com>
2025-07-28 21:39:53 +0530

    add sockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { type Phase } from "./contexts/TurnsContext";
-import { webSocketInstance } from "./socket-client";
+import { TurnsContext, type Phase } from "./contexts/TurnsContext";
+import { webSocketInstance as ws } from "./socket-client";
+import Game from "./Game";
 function App() {
-  type Player = {
-    id: string;
-    name: string;
-  };
-
-  const [player, setPlayer] = useState<Player | null>();
+  const [name, setName] = useState<string | undefined>();
+  const [players, setPlayers] = useState<string[] | null>();
   const [log, setLog] = useState<string[]>([]);
   const [phase, setPhase] = useState<Phase | null>(null);
   const [connected, setConnected] = useState<boolean>(false);
 
+  const [showGame, setShowGame] = useState(false);
   const playerAssigned = (data) => {
     console.log("Player assigned:", data);
   };
 
   const resetGame = () => {};
 
-  const connect = () => {};
-  useEffect(() => {}, []);
+  const connect = () => {
+    console.assert(name, "name is not populated, cannot connect");
+    sessionStorage.setItem("player-name", name!);
+    console.log(name);
+    ws.emit("player:connect", name, console.log);
+  };
+
+  ws.on("player:list", (players: string[]) => {
+    console.log("player list", players);
+    setPlayers(players);
+  });
+
+  ws.on("ready", () => {
+    console.log("lobby ready...");
+    setShowGame(true);
+  });
+  useEffect(() => {
+    setName(sessionStorage.getItem("player-name") || "");
+  }, []);
 
   return (
     <div className="App">
@@ -31,19 +46,20 @@ function App() {
       <input
         type="text"
         placeholder="Your name"
+        value={name}
         onChange={(e) => {
           console.log("setting player name", e.target);
-          setPlayer((prev) =>
-            prev ? { ...prev, name: e.target.value } : null
-          );
+          setName(e.target.value);
         }}
       />
       <button onClick={() => connect()}>Connect</button>
       <h1>Battle-a-bit</h1>
-      {/* <TurnsContext.Provider value={{ phase, setPhase }}>
-        <Game />
-      </TurnsContext.Provider> */}
-      {player && <p>You are Player {player.name}</p>}
+      <div>
+        <TurnsContext.Provider value={{ phase, setPhase }}>
+          {showGame && <Game />}
+        </TurnsContext.Provider>
+      </div>
+      {players && <p>List of Players: {players.map((name) => name)}</p>}
       {/* {player && <Game />} */}
       <div style={{ marginTop: 20 }}>
         <h3>Opponent Moves</h3>
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -5,7 +5,7 @@ const webSocketInstance = io("http://localhost:8080");
 webSocketInstance.on("connect", () => {
   console.log(webSocketInstance.id); // x8WIv7-mJelg7on_ALbx
 });
-webSocketInstance.on("hello", (data) => {
+webSocketInstance.on("connect:success", (data) => {
   console.log("Received hello:", data);
 });
 export { webSocketInstance };
